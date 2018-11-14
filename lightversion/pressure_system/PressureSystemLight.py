51ec610253fd3584ee26dead51f3c0a8d6960de0
Rodrigo Medeiros Guercio <rodrigo.guercio@LNLS55-linux.abtlus.org.br>
2018-11-14 15:13:05 -0200

    interface light 2

--- lightversion/pressure_system/PressureSystemLight.py
+++ lightversion/pressure_system/PressureSystemLight.py
@@ -55,6 +55,10 @@ class PressureSystem(object):
         (1) --- (1) --- (1): User interface based on PyDM channels
         '''
         self.setGuiAnd2flow() #User interface based on PyDM channels
+        '''
+        (2) --- (2) --- (2): Signals linked to data processing to determine pressure, position and temperature
+        '''
+        self.signalsCalc() #Signals linked to data processing to determine pressure, position and temperature
         
     '''
     (1) --- (1) --- (1): User interface based on PyDM channels
@@ -122,6 +126,165 @@ class PressureSystem(object):
 
         self.ui.lblLuminescenceTotal.channel = urlname + ":TotalLuminescence"
     
+    
+    '''
+    (2) --- (2) --- (2): Signals linked to data processing to determine pressure, position and temperature
+    '''  
+        
+        
+    def signalsCalc(self):
+        ''' Determination of pressure data regardless of graph'''
+        self.data = DataCalculate()
+        
+        ''' Initial setup '''
+        self.ui.PyDMPushButton_setSV.clicked.connect(self.standard)
+        
+        ''' Temp ''' 
+        self.temp = '300'
+        self.ui.LineEdit_tempBase.editingFinished.connect(self.temperatureBase)
+        
+        ''' Presssure - Position - Temperature '''
+        self.ui.PyDMPushButton_tempCalc.clicked.connect(self.temperature)
+        
+        self.ui.PyDMPushButton_PositionCalc.clicked.connect(self.position)
+        
+        self.ui.PyDMPushButton_PressureCalc.clicked.connect(self.pressure)
+        
+        self.ui.PyDMPushButton.clicked.connect(self.getDataFromRealTime)
+        
+        ''' Plotting line on graph '''
+        self.ui.Checkbox2pPltPosition.clicked.connect(self.plotRealTimeLineDesired)
+        
+    
+            
+    def standard(self):
+        '''Standard values / initial condition '''
+        temp0 = self.ui.LineEdit_tempBase_2.text() #In the same widget of others values of position and pressure
+        position10 = self.ui.LineEdit_1nm_2.text()
+        position20 = self.ui.LineEdit_2nm_2.text()
+        self.data.standard(tempzero = temp0, peak1zero = position10, peak2zero = position20)
+        self.graphdata.standard(tempzero = temp0, peak1zero = position10, peak2zero = position20)
+    
+    def temperatureBase(self):
+        ''' Temperature: Base of calculation according to sensor on OceanSpectrometer'''
+        try:
+            #temp_int = float(self.ui.LineEdit_tempBase.text())  
+            self.temp = self.ui.LineEdit_tempBase.text()
+            #self.getTemp4auto() # With lakeshore this line should commented
+            self.pressure()
+            self.position()
+        except OSError as err:
+            self.showDialog("Temperature value is not valid",err)
+    
+        
+    def temperature(self):
+        self.showDialog( title = 'Range of Temperature', text = 'Temperature determination has range between 10 and 100K using peak intensities')
+        count1 = self.ui.LineEdit_1Count.text()
+        count2 = self.ui.LineEdit_2Count.text() 
+        
+        if (self.data.temperatureCalculate(intensity_peak1 = count1, intensity_peak2 = count2)):
+            self.ui.PyDMLabel_temp_result.setText(str(self.data.temp)+' K')
+        else:     
+            self.ui.PyDMLabel_temp_result.setText("Error")
+          
+    
+    def position(self):
+        pressure1 = self.ui.LineEdit_1GPa.text()
+        pressure2 = self.ui.LineEdit_2GPa.text()
+        if (self.data.peakPositionCalculate(temp = self.temp, press1 = pressure1, press2 = pressure2)):
+            self.ui.PyDMLabel_1nm_result.setText(str(self.data.peak1) + ' nm')
+            self.ui.PyDMLabel_2nm_result.setText(str(self.data.peak2) + ' nm')
+            self.dPressure = float(pressure2)
+            self.plotCalcPosition(self.data.peak2)
+        else:
+            self.ui.PyDMLabel_1nm_result.setText('Error')
+            self.ui.PyDMLabel_2nm_result.setText('Error')
+            self.plotCalcPosition(0)
+            
+    def plotRealTimeLineDesired(self):
+        wave_on_2peak = self.ui.PyDMLabel_2nm_result.text() 
+        if (wave_on_2peak  == 'Error'):
+            self.plotCalcPosition(wl_ = 0) 
+        else:
+            idx_name = wave_on_2peak.find('n')
+            if idx_name > 0:
+                peak2 = wave_on_2peak[0:idx_name-1]
+                self.plotCalcPosition(float(peak2))
+            else:
+                self.plotCalcPosition(0)
+        
+    
+    def plotCalcPosition(self,wl_):
+        if (self.ui.Checkbox2pPltPosition.isChecked()):
+            self.plotFlagDesired = True
+            self.plotLine_nm_OnGraph(w_l = wl_,_label='SetPoint',plot=self.plotFlagDesired, _angle = 90)
+        else:
+            self.plotFlagDesired = False
+            self.plotLine_nm_OnGraph(w_l = wl_,_label='SetPoint',plot = self.plotFlagDesired, _angle = 90)
+    
+        
+    def plotLine_nm_OnGraph(self,plot = True, w_l = 694.26, _label= 'PV', _angle = 90 ):
+        try:
+            if (plot):
+                if ((w_l>600) and (w_l<800)):
+                    if self.ui.chkDark.isChecked():
+                        self.ui.wvRaw.plotItem.removeItem(self.line_calc)
+                        self.ui.wvDark.plotItem.removeItem(self.line_calc)
+                        self.line_calc = InfiniteLine(pos=w_l,angle=_angle,pen = mkPen('g',width = 2), label = _label)
+                        self.ui.wvDark.plotItem.addItem(self.line_calc)
+                    else:
+                        self.ui.wvRaw.plotItem.removeItem(self.line_calc)
+                        self.ui.wvDark.plotItem.removeItem(self.line_calc)
+                        self.line_calc = InfiniteLine(pos=w_l,angle=_angle,pen = mkPen('g',width = 2), label = _label)
+                        self.ui.wvRaw.plotItem.addItem(self.line_calc)
+                else:
+                    self.showDialog("Error plotting procedure","Wavelength is out of range")
+            else:
+                self.ui.wvRaw.plotItem.removeItem(self.line_calc)
+                self.ui.wvDark.plotItem.removeItem(self.line_calc)
+            
+        except:
+            self.showDialog("Error on pressure data processing","Mode Automatic: Error on pressure data plotting")    
+                        
+    def pressure(self):
+        position1 = self.ui.LineEdit_1nm.text()
+        position2 = self.ui.LineEdit_2nm.text()
+        if (self.data.pressureCalculate(temp = self.temp, peak1 = position1, peak2 = position2)):
+            self.ui.PyDMLabel_1Gpa_result.setText(str(self.data.press1) + ' GPa')
+            self.ui.PyDMLabel_2Gpa_result.setText(str(self.data.press2) + ' GPa')
+        else:
+            self.ui.PyDMLabel_1Gpa_result.setText('Error')
+            self.ui.PyDMLabel_2Gpa_result.setText('Error')
+    
+    def getDataFromRealTime(self):
+        ''' Set initial values of pressure calculation '''
+        try:
+            if (self.ui.chkAuto.isChecked()) and (self.wavelength is not None):
+                #temp0 = self.ui.LineEdit_tempBase_2.text()
+                temp0 = self.ui.LineEdit_tempBase.text()
+                position10 = self.wavelength[1]
+                position20 = self.wavelength[0]
+                dataFlag = self.data.standard(tempzero = temp0, peak1zero = position10, peak2zero = position20)
+                datagraphFlag = self.graphdata.standard(tempzero = temp0, peak1zero = position10, peak2zero = position20)
+                if dataFlag and datagraphFlag:
+                    self.ui.LineEdit_1nm_2.setText(str(position10))
+                    self.ui.LineEdit_2nm_2.setText(str(position20))
+                    self.ui.LineEdit_tempBase_2.text(temp0)
+                else:
+                    self.showDialog('Fatal Error on initial value settings','Fatal Error on initial value settings')
+            else:
+                self.showDialog("Error: wavelength is None or search peaks is not checked",'Error: wavelength is None or search peaks is not checked')
+        except OSError as err:
+            self.showDialog("Error: Initial value settings",err)
+
+    
+    def showDialog(self,title = None, text = None):
+        """Show a simple message dialog"""
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(text)
+        msg.setWindowTitle(title)
+        msg.exec_()  
     
 if __name__ == "__main__":  
     import sys
